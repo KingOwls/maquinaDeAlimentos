26280c3a8e9cb7933ab325c2890e427854d9c5e8
KingOwls <147095998+KingOwls@users>
2024-02-14 20:56:53 -0500

    Ultimo

---
+++ JS/main.js
@@ -0,0 +1,48 @@
+const productos = {
+    A: 270,
+    B: 340,
+    C: 390
+  };
+  
+  function calcularVuelto(precio, montoIngresado) {
+    let vuelto = montoIngresado - precio;
+    const monedas = [100, 50, 10];
+    const vueltoEnMonedas = [];
+  
+    for (const moneda of monedas) {
+      while (vuelto >= moneda) {
+        vuelto -= moneda;
+        vueltoEnMonedas.push(moneda);
+      }
+    }
+  
+    return vueltoEnMonedas;
+  }
+  
+  function comprarProducto() {
+    const producto = prompt("Elija producto (A: 270, B: 340, C: 390)").toUpperCase();
+  
+    if (productos.hasOwnProperty(producto)) {
+      const precioProducto = productos[producto];
+      let montoIngresado = 0;
+  
+      while (montoIngresado < precioProducto) {
+        const monedaIngresada = parseInt(prompt(`Ingrese moneda 10 = (${montoIngresado + 10}) 50 = (${montoIngresado + 50}) 100 = (${montoIngresado + 100}) Cantidad total a pagar: (${montoIngresado})`));
+  
+        if (![10, 50, 100].includes(monedaIngresada)) {
+          alert("Moneda no válida. Ingrese 10, 50 o 100.");
+          continue;
+        }
+  
+        montoIngresado += monedaIngresada;
+      }
+  
+      const vueltoEnMonedas = calcularVuelto(precioProducto, montoIngresado);
+      alert(`Su vuelto: ${vueltoEnMonedas.join(' ')}`);
+    } else {
+      alert("Producto no válido. Elija entre A, B o C.");
+    }
+  }
+  
+  comprarProducto();
+  
